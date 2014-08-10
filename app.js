fa1876b0841caad56d2f8d9773a4f0c594e4622f
Gavin King <gavin.king@fronter.com>
2014-08-10 18:37:37 +0200

    First stage. Simple example done.

---
+++ app.js
@@ -0,0 +1,17 @@
+function* iterator(startValue) {
+  while (true) {
+    yield startValue += 1;
+    console.log('boom')
+  }
+}
+
+// Now we assign the generated function to a variable
+var iterateFrom5 = iterator(1);
+
+// console.log(iterateFrom5()); // object is not a function
+console.log(iterateFrom5); // returns an object literal
+
+console.log(iterateFrom5.next()); // returns an object { value: 2, done: false }
+console.log(iterateFrom5.next().value); // returns 3
+console.log(iterateFrom5.next().value); // returns 4
+console.log(iterateFrom5.next().value); // returns 5
